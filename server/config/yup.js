1183bf7440475f0ffb9e13bb2700dd9a914f2456
Tushar Maharana <tusharmaharana6@gmail.com>
2020-09-25 02:37:40 +0530

    refactor(yup.js): improved schema logic for yup validation

--- server/config/yup.js
+++ server/config/yup.js
@@ -1,15 +1,17 @@
 const yup = require("yup");
 
-const signupSchema = yup.object().shape({
+const signup = {
   name: yup.string().min(3).max(50).trim().required(),
   email: yup.string().min(5).max(255).email().required(),
   password: yup.string().min(8).max(255).required(),
-});
+};
 
-const signinSchema = yup.object().shape({
+const signin = {
   email: yup.string().min(5).max(255).email().required(),
   password: yup.string().min(8).max(255).required(),
-});
+};
 
-exports.signupSchema = signupSchema;
-exports.signinSchema = signinSchema;
+const getSchema = (body) => yup.object().shape(body).strict(true).noUnknown();
+
+exports.signupSchema = getSchema(signup);
+exports.signinSchema = getSchema(signin);
