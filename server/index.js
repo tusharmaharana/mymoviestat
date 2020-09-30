8eb3b352395d74cb3a7b725eb69f496c006c93f3
Tushar Maharana <tusharmaharana6@gmail.com>
2020-09-30 06:16:21 +0530

    refactor: refactored some code

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import passport from 'passport';
 import keys from './config/keys';
 import authRoutes from './routes/authRoutes';
+import listRoutes from './routes/listRoutes';
 import './services/passport';
 
 const app = express();
@@ -12,7 +13,8 @@ mongoose
   .connect(keys.dbHost, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.log('Error while connecting to MongoDB', err));
@@ -36,5 +38,6 @@ app.use('/api/auth', authRoutes);
 app.get('/api/me', (req, res) => {
   res.send(req.user);
 });
+app.use('/api/list', listRoutes);
 
 app.listen(keys.port, () => console.log(`Listening on PORT ${keys.port}`));
--- server/middleware/validation.js
+++ server/middleware/validation.js
@@ -1,10 +1,13 @@
+import * as yup from 'yup';
+
 export const validate = schema => {
   return async (req, res, next) => {
     try {
-      const body = await schema.validate(req.body);
+      const body = await yup.object().shape(schema).strict(true).noUnknown().validate(req.body, { abortEarly: false });
       req.body = body;
       next();
     } catch (err) {
+      console.error(err);
       res.status(400).send(err.message);
     }
   };
