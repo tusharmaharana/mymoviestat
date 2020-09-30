ef93b10796835d3e1e0ea5b071ce8a41a4bdc88a
Tushar Maharana <tusharmaharana6@gmail.com>
2020-09-30 06:15:07 +0530

    feat: added constants folder

---
+++ server/constants/index.js
@@ -0,0 +1,12 @@
+export const StatusEnum = {
+  ['watching']: 'WATCHING',
+  ['seen']: 'SEEN',
+  ['want-to-see']: 'WANT_TO_SEE',
+  ['on-hold']: 'ON_HOLD'
+};
+
+export const TypeEnum = {
+  ['movie']: 'MOVIE',
+  ['series']: 'SERIES',
+  ['episode']: 'EPISODE'
+};
