3b8cc84ca7ce4dab5528e81595eb8ad372c92ec6
Tushar Maharana <tusharmaharana6@gmail.com>
2020-09-30 06:03:02 +0530

    feat: added the yup schema for list model

--- server/services/yup.js
+++ server/services/yup.js
@@ -1,14 +1,29 @@
 import * as yup from 'yup';
 
-const getSchema = body => yup.object().shape(body).strict(true).noUnknown();
-
-export const signupSchema = getSchema({
+export const signupSchema = {
   name: yup.string().min(3).max(50).trim().required(),
   email: yup.string().min(5).max(255).email().required(),
   password: yup.string().min(8).max(255).required()
-});
+};
 
-export const signinSchema = getSchema({
+export const signinSchema = {
   email: yup.string().min(5).max(255).email().required(),
   password: yup.string().min(8).max(255).required()
-});
+};
+
+export const recordSchema = {
+  title: yup.string().min(1).max(50).required(),
+  imdbID: yup.string().required(),
+  favourite: yup.boolean().default(false),
+  status: yup
+    .string()
+    .matches(/(WANT_TO_SEE|WATCHING|SEEN|ON_HOLD)/)
+    .notRequired(),
+  type: yup
+    .string()
+    .matches(/(MOVIE|SERIES|EPISODE)/)
+    .required(),
+  year: yup.string(),
+  imdbRating: yup.mixed().test('is-decimal', 'not a decimal number', val => !isNaN(val)),
+  poster: yup.string()
+};
