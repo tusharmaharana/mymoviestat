f1a34d17a36ccf76394228516160995a31166715
Tushar <tusharmaharana6@gmail.com>
2021-09-09 21:45:53 +0530

    fix: https omdb error

--- client/src/api/omdb.js
+++ client/src/api/omdb.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const KEY = process.env.REACT_APP_OMDB_API_KEY;
 
 const omdb = restParams =>
-  axios.get('http://www.omdbapi.com/', {
+  axios.get('https://www.omdbapi.com/', {
     params: {
       apikey: KEY,
       ...restParams
